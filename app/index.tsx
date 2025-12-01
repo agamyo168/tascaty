37652ed0afa7465f9aaaa91041325bbcd88ce8e2
agamyo168 <agamyo168@gmail.com>
2025-12-01 16:21:34 +0200

    fix: error on phone required GestureHandlerRootView

--- app/index.tsx
+++ app/index.tsx
@@ -10,8 +10,14 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 interface TaskItem {
   title: string;
 }
@@ -37,125 +43,129 @@ const HomeScreen = () => {
     setTaskItems([...taskItems, task]);
     setTask({ title: "" });
   };
+  const insets = useSafeAreaInsets();
+  const styles = StyleSheet.create({
+    header: {
+      paddingBottom: 60,
+      paddingTop: 10,
+      alignItems: "center",
+    },
+    scrollContainer: {
+      marginBottom: 60,
+    },
+    plusText: {
+      fontSize: 64,
+    },
+    plusWrapper: {
+      width: 60,
+      height: 60,
+      backgroundColor: "#cba6f7",
+      borderRadius: 60,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+
+    writeTaskWrapper: {
+      position: "absolute",
+      bottom: 0,
+      padding: 10,
+      borderTopEndRadius: 15,
+      borderTopStartRadius: 15,
+
+      backgroundColor: "#313244",
+      width: "100%",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-around",
+    },
+    input: {
+      padding: 15,
+      backgroundColor: "#45475a",
+      color: "#cdd6f4",
+      borderRadius: 60,
+      borderColor: "#9399b2",
+      borderWidth: 1,
+      width: 250,
+    },
+
+    tasksWrapper: {
+      paddingBottom: 30,
+      paddingHorizontal: 20,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: "#1e1e2e",
+      paddingBottom: insets.bottom,
+    },
+    items: {},
+    //TODO: Make section title a header/navbar instead
+    headerTitle: {
+      color: "#cdd6f4",
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+    sectionTitle: {
+      color: "#cdd6f4",
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 20,
+    },
+  });
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Page title</Text>
-      </View>
-      <ScrollView style={styles.scrollContainer}>
-        <View style={styles.tasksWrapper}>
-          <Text style={styles.sectionTitle}>Todo ({taskItems.length})</Text>
-          <View style={styles.items}>
-            {taskItems.map((task, index) => (
-              <Task
-                key={index}
-                text={task.title}
-                onPress={() => handleCompleteTask(index)}
-              />
-            ))}
-          </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Page title</Text>
         </View>
-        <View style={styles.tasksWrapper}>
-          <Text style={styles.sectionTitle}>
-            Completed ({completedTasks.length})
-          </Text>
-          <View style={styles.items}>
-            {completedTasks.map((task, index) => (
-              <Task
-                key={index}
-                done
-                text={task.title}
-                onPress={() => handleIncompleteTask(index)}
-              />
-            ))}
+        <ScrollView style={styles.scrollContainer}>
+          <View style={styles.tasksWrapper}>
+            <Text style={styles.sectionTitle}>Todo ({taskItems.length})</Text>
+            <View style={styles.items}>
+              {taskItems.map((task, index) => (
+                <Task
+                  key={index}
+                  text={task.title}
+                  onPress={() => handleCompleteTask(index)}
+                />
+              ))}
+            </View>
           </View>
-        </View>
-      </ScrollView>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput
-          style={styles.input}
-          placeholder="Write a task"
-          value={task.title}
-          onChangeText={(text) => setTask({ title: text })}
-        />
-        <TouchableOpacity onPress={handleAddTask}>
-          <View style={styles.plusWrapper}>
-            <Text style={styles.plusText}>+</Text>
+          <View style={styles.tasksWrapper}>
+            <Text style={styles.sectionTitle}>
+              Completed ({completedTasks.length})
+            </Text>
+            <View style={styles.items}>
+              {completedTasks.map((task, index) => (
+                <Task
+                  key={index}
+                  done
+                  text={task.title}
+                  onPress={() => handleIncompleteTask(index)}
+                />
+              ))}
+            </View>
           </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+        </ScrollView>
+        {/* Replace it with Bottom Sheet!! */}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
+        >
+          <TextInput
+            style={styles.input}
+            placeholder="Write a task"
+            value={task.title}
+            onChangeText={(text) => setTask({ title: text })}
+          />
+          <TouchableOpacity onPress={handleAddTask}>
+            <View style={styles.plusWrapper}>
+              <Text style={styles.plusText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
-
-const styles = StyleSheet.create({
-  header: {
-    paddingBottom: 60,
-    paddingTop: 10,
-    alignItems: "center",
-  },
-  scrollContainer: {
-    marginBottom: 60,
-  },
-  plusText: {
-    fontSize: 64,
-  },
-  plusWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: "#cba6f7",
-    borderRadius: 60,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  writeTaskWrapper: {
-    position: "absolute",
-    bottom: 0,
-    padding: 10,
-    borderTopEndRadius: 15,
-    borderTopStartRadius: 15,
-
-    backgroundColor: "#313244",
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-  },
-  input: {
-    padding: 15,
-    backgroundColor: "#45475a",
-    color: "#cdd6f4",
-    borderRadius: 60,
-    borderColor: "#9399b2",
-    borderWidth: 1,
-    width: 250,
-  },
-
-  tasksWrapper: {
-    paddingBottom: 30,
-    paddingHorizontal: 20,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#1e1e2e",
-  },
-  items: {},
-  //TODO: Make section title a header/navbar instead
-  headerTitle: {
-    color: "#cdd6f4",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  sectionTitle: {
-    color: "#cdd6f4",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-});
 
 export default HomeScreen;
